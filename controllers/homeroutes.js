1e661339971bc6cc6299dbc5bd852b5b09b7cebb
Natalie Finnegan <nfinneg2@gmail.com>
2021-05-08 11:01:47 -0600

    router export, main handlebrs wrking

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,15 +1,13 @@
-const router = require('express').Router();
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+//const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
-    try {
-       
-      // Pass serialized data and session flag into template
-      res.render('homepage', { 
-        articles, 
-        logged_in: req.session.logged_in 
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+router.get("/", async (req, res) => {
+  try {
+    // Pass serialized data and session flag into template
+    res.render("homepage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;

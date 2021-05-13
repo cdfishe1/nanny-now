7fd01a3284c739c1cac3205a476a0c3ddb452e45
Natalie Finnegan <nfinneg2@gmail.com>
2021-05-12 18:03:23 -0600

    reroute signup form

---
+++ public/js/signup-bak.js
@@ -0,0 +1,28 @@
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#newUser").value.trim();
+  const email = document.querySelector("#newUserEmail").value.trim();
+  const password = document.querySelector("#newUserPass").value.trim();
+  const location = document.querySelector("#newUserLocation").value.trim();
+
+  if (username && email && password && location) {
+    const response = await fetch("api/parents", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password, location }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to sign up.");
+    }
+  }
+};
+
+document
+  .querySelector(".sign-up")
+  .addEventListener("submit", signupFormHandler);
+
+
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,20 +1,42 @@
-const signupFormHandler = async (event) => {
+const signupRedoFormHandler = async (event) => {
   event.preventDefault();
 
   const username = document.querySelector("#newUser").value.trim();
   const email = document.querySelector("#newUserEmail").value.trim();
   const password = document.querySelector("#newUserPass").value.trim();
   const location = document.querySelector("#newUserLocation").value.trim();
+  const gender = document.querySelector("#nannyGender").value.trim();
+  const language = document.querySelector("#nannyLanguage").value.trim();
+  const education = document.querySelector("#nannyEducation").value.trim();
+  const experience = document.querySelector("#nannyExp").value.trim();
 
-  if (username && email && password && location) {
+  if (
+    username &&
+    email &&
+    password &&
+    location &&
+    gender &&
+    language &&
+    education &&
+    experience
+  ) {
     const response = await fetch("api/parents", {
       method: "POST",
-      body: JSON.stringify({ username, email, password, location }),
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+        location,
+        gender,
+        language,
+        education,
+        experience,
+      }),
       headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/search");
     } else {
       alert("Failed to sign up.");
     }
@@ -23,6 +45,4 @@ const signupFormHandler = async (event) => {
 
 document
   .querySelector(".sign-up")
-  .addEventListener("submit", signupFormHandler);
-
-
+  .addEventListener("submit", signupRedoFormHandler);

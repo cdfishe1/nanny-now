077e3fb48a2863fb4ef1660f37ef1768e44ed889
Natalie Finnegan <nfinneg2@gmail.com>
2021-05-14 19:23:56 -0600

    fix srch orderby, add shre contact info btn & js

---
+++ public/js/sharedetails.js
@@ -0,0 +1,20 @@
+// let success = document.getElementById("#contacted");
+// console.log(success);
+// success.innerText = "Your Email Was Shared Successfully!";
+
+// const shareBtn = () => {
+//   let success = document.getElementById("#contacted");
+// };
+
+let btn = document.querySelector("#contactLink");
+
+btn.addEventListener("click", () => {
+  console.log("OK");
+});
+// addEventListener("click", (event) => {
+//   console.log("OK");
+//   // let success = document.getElementById("#contacted");
+//   //console.log(success);
+//   // this.success.innerText = "Your Email Was Shared Successfully!";
+//   console.log(this);
+// });
